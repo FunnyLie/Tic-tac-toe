f5ef7ac8b65af999a43e27a35a0f43f5891f81dc
FunnyLie <funnylie25@gmail.com>
2025-09-28 23:39:15 +0300

    Implement DrawVerifier component

--- src/FunnyLie/tictactoe/components/DrawVerifier.java
+++ src/FunnyLie/tictactoe/components/DrawVerifier.java
@@ -1,9 +1,17 @@
 package FunnyLie.tictactoe.components;
 
+import FunnyLie.tictactoe.model.Cell;
 import FunnyLie.tictactoe.model.GameTable;
 
 public class DrawVerifier {
     public boolean isDraw(GameTable gameTable) {
-        return false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (gameTable.isEmpty(new Cell(i, j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
