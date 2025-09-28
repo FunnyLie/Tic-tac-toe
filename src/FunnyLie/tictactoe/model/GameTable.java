ba0fc886fbafec551f28beab38c86d2d3b30dc9e
FunnyLie <funnylie25@gmail.com>
2025-09-28 16:13:35 +0300

    Add model classes

--- src/FunnyLie/tictactoe/model/GameTable.java
+++ src/FunnyLie/tictactoe/model/GameTable.java
@@ -1,4 +1,23 @@
 package FunnyLie.tictactoe.model;
 
 public class GameTable {
+
+    private final char[][] table = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+    };
+
+    public boolean isEmpty(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()] == ' ';
+    }
+
+    public char getSSign(final Cell cell) {
+        return table[cell.getRow()][cell.getCol()];
+    }
+
+    public void setSign(final Cell cell, final char sign) {
+        table[cell.getRow()][cell.getCol()] = sign;
+    }
 }
+
