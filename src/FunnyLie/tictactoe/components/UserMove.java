0e3ad468f943d8eb499fe4ee5cb17748e93e2ec5
FunnyLie <funnylie25@gmail.com>
2025-09-28 23:24:20 +0300

    Implement UserMove component

--- src/FunnyLie/tictactoe/components/UserMove.java
+++ src/FunnyLie/tictactoe/components/UserMove.java
@@ -1,9 +1,47 @@
 package FunnyLie.tictactoe.components;
 
+import FunnyLie.tictactoe.model.Cell;
 import FunnyLie.tictactoe.model.GameTable;
 
-public class UserMove {
-    public void make(GameTable gameTable) {
+import java.util.Scanner;
 
+public class UserMove {
+
+    private final char[][] mapping = {
+            {'7', '8', '0'},
+            {'4', '5', '6'},
+            {'1', '2', '3'},
+    };
+
+
+    public void make(GameTable gameTable) {
+        final Cell cell = getUserInput();
+        while (true) {
+            if (gameTable.isEmpty((cell))) {
+                gameTable.setSign(cell, 'X');
+                return;
+            } else {
+                System.out.println("Can't make a move, because the cell is not free! Try again");
+            }
+        }
+    }
+
+    private final Cell getUserInput() {
+        while (true) {
+            System.out.println("Please type number between 1 and 9");
+            final String userInput = new Scanner(System.in).nextLine();
+            if (userInput.length() == 1) {
+                final char ch = userInput.charAt(0);
+                if (ch > '1' && ch < '9') {
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            if (mapping[i][j] == ch) {
+                                return new Cell(i, j);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
